b549cac36fcdac4275c6c136d06160af1a2d6077
vervis01 <vishalkrverma511@gmail.com>
2025-03-06 23:38:20 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,7 @@
+a=20
+b = 40
+print("sum of the a and b is ",a+b)
+
+print('''HEy DevOps Engineers welcome tom the jenkins job automation workspace. I am hare to help you. "
+        
+        Please let me know if you want anyn type information frommy side''')
